91e4848affa2dafa62fe2111604f6828a9b2e379
echav <echav@Susan>
2024-07-17 11:30:15 -0600

    Avance Susan: Desarrollar parte de la sección de damas, caballeros, calzado y accesorios.

---
+++ src/test/java/com/charlieshop/Charlieshop/CharlieShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlieshop.Charlieshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharlieShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
